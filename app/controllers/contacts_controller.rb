61d54d421f0532a35ca8b313111d51154a893d1c
Dan Mashuda <dmashuda@ycp.edu>
2015-02-12 16:53:41 -0500

    update gitignore

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,8 @@ class ContactsController < ApplicationController
   # GET /contacts.json
   def index
     if signed_in?
-      @contacts = current_user.contacts.all.limit(10)
+
+      @contacts = current_user.contacts.all.paginate :page => params[:page], :per_page => 10
     else
       redirect_to "/users/sign_in"
     end
